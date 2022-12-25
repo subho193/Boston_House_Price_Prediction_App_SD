efcc0e5e5b0498afe5fb7d15d02fd8c4fecc2844
Subhajit Dutta <subhajit.sds3@gmail.com>
2022-12-26 02:14:20 +0530

    updated details

--- app.py
+++ app.py
@@ -23,6 +23,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in (request.form.values())]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=linear_model.predict(final_input)[0]
+    return render_template("home.html",prediction_text=f"The predicted house price is : {output}")
+
 if __name__=="__main__":
     app.run(debug=True)
 
